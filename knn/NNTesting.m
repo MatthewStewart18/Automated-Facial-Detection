a33a442d0e64e7e3aff69ca1cea7959fbbf3dd65
MelzCodes <matteo.melis2012@gmail.com>
2024-10-29 20:20:54 +0000

    clean up

--- knn/NNTesting.m
+++
@@ -1,5 +0,0 @@
-function prediction = NNTesting(testImage, modelNN)
-    distances = EuclideanDistance(modelNN.neighbours, testImage);
-    [~, minIndex] = min(distances);
-    prediction = modelNN.labels(minIndex);
-end
