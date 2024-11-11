a1814ac9f8294329e2bc32fa540dd0cd7d67a235
MelzCodes <matteo.melis2012@gmail.com>
2024-11-11 20:24:57 +0000

    knn edges

--- models/knn/knnEdges.m
+++ models/knn/knnEdges.m
@@ -9,20 +9,21 @@ numTrainingImages = size(trainingImages, 1);
 numTestImages = size(testingImages, 1);
 fprintf('Loaded augmented training set: %d images\n', size(trainingImages,1));
 
+trainingEdges = extractEdges(trainingImages);
+
 % Train model
 fprintf('Training KNN model on entire image ...\n')
-modelKNN = NNtraining(trainingImages, trainingLabels);
+modelKNN = NNtraining(trainingEdges, trainingLabels);
 
 % Set K to sqrt(N)
 K = round(sqrt(numTrainingImages)); 
-% this isnt working great, using 50 temporarily
-% K = 50;
+testEdges = extractEdges(testingImages);
 
 fprintf('Getting model predictions for K = %d\n', K);
 predictions = zeros(numTestImages);
 for i = 1:numTestImages
-    testImage = testingImages(i, :);
-    predictions(i, 1) = KNNTesting(testImage, modelKNN, K);
+    testEdge = testEdges(i, :);
+    predictions(i, 1) = KNNTesting(testEdge, modelKNN, K);
 end
 
 fprintf('Evaluating model predictions...\n');
