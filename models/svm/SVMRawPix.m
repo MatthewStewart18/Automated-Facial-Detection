9c166ca0514aef7c494c35a943af45192a1b6b35
MelzCodes <matteo.melis2012@gmail.com>
2024-11-17 23:30:25 +0000

    update raw pix svm

--- models/svm/SVMRawPix.m
+++ models/svm/SVMRawPix.m
@@ -1,32 +1,25 @@
 % SVMrawpix.m
 clear all
 close all
-
-addpath ../../images
 addpath SVM-KM
+addpath ../../images
+addpath ../../utils
+addpath ../../feature-extraction-utils
+addpath ../../preprocessing-utils
 
 % Load training and test data
 [trainingImages, trainingLabels] = loadFaceImages('../../images/face_train.cdataset');
 [testingImages, testingLabels] = loadFaceImages('../../images/face_test.cdataset');
-numTrainingImages = size(trainingImages, 1);
-numTestImages = size(testingImages, 1);
-trainingImages = normalize(trainingImages, 'zscore');
-testingImages = normalize(testingImages, 'zscore');
-fprintf('Loaded augmented training set: %d images\n', size(trainingImages,1));
 
 % Train model
-fprintf('Training SVM model on entire image ...\n')
-fprintf('Training KNN model on entire image ...\n')
-modelSVM = SVMtraining(trainingImages, trainingLabels);
+params = struct('lambda', 1e-20, 'C', Inf, 'kerneloption', 6, 'kernel', 'poly');
+modelSVM = SVMtraining(trainingImages, trainingLabels, params);
 
-fprintf('Getting model predictions for test set\n');
-predictions = zeros(numTestImages, 1);
-for i = 1:numTestImages
-    testImage = testingImages(i, :);
-    predictions(i) = SVMTesting(testImage, modelSVM);
-end
+predictions = extractPredictionsSVM(testingImages, modelSVM);
 
 fprintf('Evaluating model predictions...\n');
-[accuracy, precision, recall, f1_score] = calculateMetrics(predictions, testingLabels);
+[~] = calculateMetrics(predictions, testingLabels);
 
-save('modelSVM.mat', 'modelSVM');
+% Display images of the correct/incorrect predictions
+dispPreds(predictions, testingLabels, testingImages);
+% save('modelSVM.mat', 'modelSVM');
