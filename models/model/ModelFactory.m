6ea9c1456ff9b14d965561528c5d2075c2dca5df
MelzCodes <matteo.melis2012@gmail.com>
2024-11-25 22:12:58 +0000

    fix random forest

--- models/model/ModelFactory.m
+++ models/model/ModelFactory.m
@@ -9,12 +9,13 @@ classdef ModelFactory
         Params               % Model parameters
         TrainingFunction     % Training function
         PredictionFunction   % Prediction function
-        PCAParameters        % Struct to store PCA parameters (eigenVectors, meanX)
+        PCAParameters        % Struct to store PCA parameters later (eigenVectors, meanX)
+        RFNumTrees           % Num of trees for RF model (optional param)
     end
     
     methods
         % Constructor
-        function obj = ModelFactory(modelType, featureType, preprocessingType, params)
+        function obj = ModelFactory(modelType, featureType, preprocessingType, params, RFnumTrees)
             obj.ModelType = modelType;
             obj.FeatureType = featureType;
             obj.Params = params;
@@ -22,6 +23,11 @@ classdef ModelFactory
             obj.FeatureExtractors = {};
             obj.PreprocessingSteps = {};
             obj.PCAParameters = struct();
+            if nargin < 5
+                obj.RFNumTrees = 0;
+            else 
+                obj.RFNumTrees = RFnumTrees;
+            end
             
             % Map ModelType to Training and Prediction Functions
             switch modelType
@@ -33,6 +39,9 @@ classdef ModelFactory
                     obj.PredictionFunction = @KNNTesting;
                 case ModelType.LG
                     obj.TrainingFunction = @fitglm;
+                    obj.PredictionFunction = @predict;
+                case ModelType.RF
+                    obj.TrainingFunction = @TreeBagger;
                     obj.PredictionFunction = @predict;
                 otherwise
                     error('Unsupported ModelType');
@@ -157,7 +166,9 @@ classdef ModelFactory
             [features, obj] = obj.applyFeatureExtraction(trainImages, true);
             
             % Train the model using the specified training function
-            if isempty(obj.Params)
+            if obj.ModelType == ModelType.RF
+                obj.Model = obj.TrainingFunction(obj.RFNumTrees, features, trainLabels);
+            elseif isempty(obj.Params)
                 obj.Model = obj.TrainingFunction(features, trainLabels);
             else
                 obj.Model = obj.TrainingFunction(features, trainLabels, obj.Params);
@@ -176,10 +187,13 @@ classdef ModelFactory
             defaultConfidence = 1;
             
             % Make predictions using the specified prediction function
-            if obj.ModelType == ModelType.LG 
+            if obj.ModelType == ModelType.LG  
                 predictions = obj.PredictionFunction(obj.Model, testFeatures);
                 confidence = double(predictions);
                 predictions = double(predictions >= 0.5);
+            elseif obj.ModelType == ModelType.RF
+                [predictions, confidence, ~] = obj.PredictionFunction(obj.Model, testFeatures);
+                predictions = str2double(predictions);
             else
                 if nargout(obj.PredictionFunction) > 1
                     [predictions, confidence] = obj.PredictionFunction(testFeatures, obj.Model);
@@ -191,9 +205,9 @@ classdef ModelFactory
         end
         
         % Evaluate the model
-        function evaluate(~, predictions, testLabels, testImages)
+        function [accuracy, confusion_matrix] = evaluate(~, predictions, testLabels, testImages)
             fprintf('Evaluating model predictions...\n');
-            [~] = calculateMetrics(predictions, testLabels);
+            [accuracy, ~, ~, ~, confusion_matrix] = calculateMetrics(predictions, testLabels);
             dispPreds(predictions, testLabels, testImages);
         end
     end
--- models/model/modelFactoryImpl.m
+++ models/model/modelFactoryImpl.m
@@ -16,8 +16,8 @@ addpath ../../preprocessing-utils
 [test_images, test_labels] = loadFaceImages('../../images/face_test.cdataset');
 
 % Set current model and feature configurations
-modelType = ModelType.KNN;
-featureType = FeatureType.GaborPCA;
+modelType = ModelType.SVM;
+featureType = FeatureType.PCA;
 preprocessingType = PreprocessingType.HistEq;
 
 % Define model parameters
@@ -30,21 +30,28 @@ switch modelType
         params = {};
         train_labels(train_labels == -1) = 0;
         test_labels(test_labels == -1) = 0;
+    case ModelType.RF
+        numTrees = 350;
+        params = {};
     otherwise
         error('Unsupported ModelType');
 end
 
 % Create the model object
-model = ModelFactory(modelType, featureType, preprocessingType, params);
+if modelType == ModelType.RF
+    model = ModelFactory(modelType, featureType, preprocessingType, params, numTrees);
+else 
+    model = ModelFactory(modelType, featureType, preprocessingType, params);
+end
 
 % Train the model
 model = model.train(train_images, train_labels);
 
 % Test the model
-predictions = model.test(test_images);
+[predictions, confidence] = model.test(test_images);
 
 % Evaluate the model
-model.evaluate(predictions, test_labels, test_images);
+[~, ~] = model.evaluate(predictions, test_labels, test_images);
 
 % Save the trained model
 savePath = sprintf('saved-models/%s/%s_%s_Model.mat', ...
